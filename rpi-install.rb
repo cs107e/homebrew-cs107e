221995320ed3a2f65a633689f69d017f9dce33b0
Sergio Benitez <sbenitez@mit.edu>
2016-02-09 23:14:22 -0800

    Update rpi-install Ruby script.

--- rpi-install.rb
+++ rpi-install.rb
@@ -2,8 +2,8 @@ class RpiInstall < Formula
   desc "Raspberry Pi uploading script for cs107e"
   homepage "https://cs107e.github.io/guides/mac_toolchain/"
   url "https://cs107e.github.io/guides/mac_toolchain/rpi-install.tar.bz2"
-  version "0.1-cs170e"
-  sha256 "bd3a44737f66aaaf5fbc64a23a8e3bdc8b1b996dd8f09569c56d3761759d31ae"
+  version "0.2-cs170e"
+  sha256 "fda740f7592a7cf55f9dff1122df14cd3c83a1594a39b0f37fdbb38e07cd926b"
   depends_on "pyserial" => [:python, "serial"]
   depends_on "xmodem" => :python
 
