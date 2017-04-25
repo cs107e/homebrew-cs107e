16a9fef9cdcefe5370c0045c3beb0cc217aea427
Omar Rizwan <omar.rizwan@gmail.com>
2017-04-24 18:35:35 -0700

    rpi-install: Update to 0.4.

--- rpi-install.rb
+++ rpi-install.rb
@@ -2,8 +2,8 @@ class RpiInstall < Formula
   desc "Raspberry Pi uploading script for cs107e"
   homepage "https://cs107e.github.io/guides/mac_toolchain/"
   url "https://cs107e.github.io/guides/mac_toolchain/rpi-install.tar.bz2"
-  version "0.3-cs107e"
-  sha256 "33f0b7696520e0b50d252fce7912365724585088ce5b5cf5473a72c3313d7ae9"
+  version "0.4-cs107e"
+  sha256 "a5c7ba34177d97f4cc11254b02743dbd5e48721ba5cb9f01c146b1764b263f35"
   depends_on "pyserial" => [:python, "serial"]
   depends_on "xmodem" => :python
 
