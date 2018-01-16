9628da371d8f9404f13e9873e448dbe9a786138c
Julie Zelenski <zelenski@cs.stanford.edu>
2018-01-15 16:25:44 -0800

    shasum 256 please!

--- rpi-install.rb
+++ rpi-install.rb
@@ -3,7 +3,7 @@ class RpiInstall < Formula
   homepage "https://cs107e.github.io/guides/mac_toolchain/"
   url "https://cs107e.github.io/guides/mac_toolchain/rpi-install.tar.bz2"
   version "0.8-cs107e"
-  sha256 "4c40e7fe14682fc23760f75a0533b4499aa2e193"
+  sha256 "0fa94feb78ddd10add929be4df73a4bab3b0be838790b44b5026fc3916df9ef9"
   depends_on "pyserial" => [:python, "serial"]
   depends_on "xmodem" => :python
 
