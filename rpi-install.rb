4bf4f10137eb6d3dd264a4b1dad9c60ebdb5c8ab
Julie Zelenski <zelenski@cs.stanford.edu>
2018-01-15 16:07:31 -0800

    Update shasum in brew tap

--- rpi-install.rb
+++ rpi-install.rb
@@ -2,8 +2,8 @@ class RpiInstall < Formula
   desc "Raspberry Pi uploading script for cs107e"
   homepage "https://cs107e.github.io/guides/mac_toolchain/"
   url "https://cs107e.github.io/guides/mac_toolchain/rpi-install.tar.bz2"
-  version "0.5-cs107e"
-  sha256 "b3cd041c27ba0c9fede45dc007fa5657fadec2b3f8c3aa71901b1c3ac5241216"
+  version "0.8-cs107e"
+  sha256 "4c40e7fe14682fc23760f75a0533b4499aa2e193"
   depends_on "pyserial" => [:python, "serial"]
   depends_on "xmodem" => :python
 
