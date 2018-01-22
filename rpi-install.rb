d7f086b483a4ac33c605c497196af57b19b5bb0a
Julie Zelenski <zelenski@stanford.edu>
2018-01-22 13:52:50 -0800

    Oops, add correct shasum

--- rpi-install.rb
+++ rpi-install.rb
@@ -3,6 +3,7 @@ class RpiInstall < Formula
   homepage "https://cs107e.github.io/guides/mac_toolchain/"
   url "https://cs107e.github.io/guides/mac_toolchain/rpi-install.py"
   version "0.8-cs107e"
+  sha256 "25b5cf442964def2bf5c0a9c57b221799104131da9b0bfcb5cb95d16e0f456ab"
   sha256 "https://github.com/cs107e/homebrew-cs107e.git"
 
   def install
