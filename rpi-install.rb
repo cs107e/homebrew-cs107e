e48253dde219703fe50265433151e2eaf80d7205
Julie Zelenski <zelenski@stanford.edu>
2018-01-22 13:45:50 -0800

    Don't pack rpi-install as archive, just single text file emove the python dependencies until we can get brew reliably validating them

--- rpi-install.rb
+++ rpi-install.rb
@@ -1,11 +1,9 @@
 class RpiInstall < Formula
   desc "Raspberry Pi uploading script for cs107e"
   homepage "https://cs107e.github.io/guides/mac_toolchain/"
-  url "https://cs107e.github.io/guides/mac_toolchain/rpi-install.tar.bz2"
+  url "https://cs107e.github.io/guides/mac_toolchain/rpi-install.py"
   version "0.8-cs107e"
-  sha256 "0fa94feb78ddd10add929be4df73a4bab3b0be838790b44b5026fc3916df9ef9"
-  depends_on "pyserial" => [:python, "serial"]
-  depends_on "xmodem" => :python
+  sha256 "https://github.com/cs107e/homebrew-cs107e.git"
 
   def install
     bin.install "rpi-install.py"
