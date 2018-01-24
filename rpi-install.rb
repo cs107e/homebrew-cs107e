9c8fb54777a90f78e6b33fba0f961daaa6e25e23
Julie Zelenski <zelenski@cs.stanford.edu>
2018-01-23 16:14:06 -0800

    Remove extraneous/conflicting sum

--- rpi-install.rb
+++ rpi-install.rb
@@ -4,7 +4,6 @@ class RpiInstall < Formula
   url "https://cs107e.github.io/guides/mac_toolchain/rpi-install.py"
   version "0.8-cs107e"
   sha256 "25b5cf442964def2bf5c0a9c57b221799104131da9b0bfcb5cb95d16e0f456ab"
-  sha256 "https://github.com/cs107e/homebrew-cs107e.git"
 
   def install
     bin.install "rpi-install.py"
