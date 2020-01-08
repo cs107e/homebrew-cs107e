5d47fcb395cdc5957f395d3927c11e8301325a92
Julie Zelenski <zelenski@users.noreply.github.com>
2020-01-08 00:02:58 -0800

    Update arm-none-eabi.rb

--- arm-none-eabi.rb
+++ arm-none-eabi.rb
@@ -2,8 +2,8 @@ require "FileUtils"
 
 class ArmNoneEabi < Formula
   desc "ARM toolchain for cs107e"
-  homepage "https://cs107e.github.io/guides/mac_toolchain/"
-  url "https://cs107e.github.io/guides/mac_toolchain/arm-none-eabi.tar.bz2"
+  homepage "https://cs107e.github.io/guides/install/"
+  url "https://cs107e.github.io/guides/install/mac-arm-none-eabi.tar.bz2"
   version "4.8.3-cs170e"
   sha256 "b906822314a87f97468d3a7d70fa10d241a5f73c5ce03b54304cdd84458da7b4"
 
