553892c60d5b0f200e98466e61d10f864a521329
Julie Zelenski <zelenski@cs.stanford.edu>
2020-12-30 16:31:38 -0800

    Preserve formula for 4.8 version of toolchain

---
+++ arm-none-eabi4.rb
@@ -0,0 +1,14 @@
+require "FileUtils"
+
+class ArmNoneEabi < Formula
+  desc "ARM toolchain for cs107e"
+  homepage "https://cs107e.github.io/guides/install/"
+  url "https://cs107e.github.io/guides/install/mac-arm-none-eabi.tar.bz2"
+  version "4.8.3-cs170e"
+  sha256 "b906822314a87f97468d3a7d70fa10d241a5f73c5ce03b54304cdd84458da7b4"
+
+  def install
+    ohai "Copying binaries..."
+    cp_r %w[arm-none-eabi bin include lib libexec share], "#{prefix}/"
+  end
+end
