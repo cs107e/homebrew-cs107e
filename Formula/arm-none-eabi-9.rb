6d3a28ff8194cbab5cf78461c9fb6a3ad87f48f3
Julie Zelenski <zelenski@cs.stanford.edu>
2020-12-30 20:01:53 -0800

    Fix shasum

--- Formula/arm-none-eabi-9.rb
+++ Formula/arm-none-eabi-9.rb
@@ -4,7 +4,7 @@ class ArmNoneEabi9 < Formula
   homepage "https://cs107e.github.io/guides/install/"
   url "https://github.com/cs107e/homebrew-cs107e/raw/master/mac-arm-none-eabi-9.tar.bz2"
   version "9-2019q4-cs107e"
-  sha256 "ef7feafee49762af38adb3a183a36df3d00f0d6ecd5ca89baca481513eeca1cc"
+  sha256 "8244c5ceda63a940a9613f49fddf4a40feda4b475502eeb6a0075beab52f6c10"
 
   def install
     ohai "Copying binaries..."
