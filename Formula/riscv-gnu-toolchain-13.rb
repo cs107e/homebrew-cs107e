199564616b9f34706cab2075e9689e5123e5479b
Kenny Oseleononmen <kenny1g@cs.stanford.edu>
2024-01-03 13:27:16 -0500

    Updated hash for arm binary

--- Formula/riscv-gnu-toolchain-13.rb
+++ Formula/riscv-gnu-toolchain-13.rb
@@ -5,7 +5,7 @@ class RiscvGnuToolchain13 < Formula
 
   if Hardware::CPU.arm?
     url "https://github.com/cs107e/homebrew-cs107e/raw/master/riscv-gnu-toolchain-arm.13.tar.gz"
-    sha256 "a1a634d881117f969209dbd7ecf02569f754c0af58e1e6b95bfc25da3ff1ac7c"
+    sha256 "abdbb79e91750b1906fc2820f3ab5c133d89ce3283dec333ec7b56129f858636"
   else
     url "https://github.com/cs107e/homebrew-cs107e/raw/master/riscv-gnu-toolchain-intel.13.tar.gz"
     sha256 "dae252a926c582f65102b58aef1cec364bdbf5d4d8bdd2724ca9b7327fe97988"
