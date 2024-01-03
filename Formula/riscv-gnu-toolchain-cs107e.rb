db8034833c3826a97e7569c2edff6e224623b2d0
Kenny Oseleononmen <kenny1g@cs.stanford.edu>
2024-01-03 12:44:30 -0500

    New bottle names

--- Formula/riscv-gnu-toolchain-cs107e.rb
+++ Formula/riscv-gnu-toolchain-cs107e.rb
@@ -1,8 +1,7 @@
 class RiscvGnuToolchainCs107e < Formula
   desc "Pre-built RISC-V GNU toolchain for CS107e"
   homepage "https://cs107e.github.io/guides/install/"
-  version "12-2023q1-cs107e"
-  # Below url and sha256 are not used, but are required by Homebrew
+  version "12-2023q1"
   url "https://github.com/cs107e/homebrew-cs107e/raw/master/riscv-gnu-toolchain.zip"
   sha256 "a1a634d881117f969209dbd7ecf02569f754c0af58e1e6b95bfc25da3ff1ac7c"
 
@@ -15,7 +14,11 @@ class RiscvGnuToolchainCs107e < Formula
   end
 
   def install
-    # Empty as we are using pre-built binaries
+    # We hope to have bottles for all platforms, this is a failsafe 
+    # Homebrew extracts tarballs automatically, so just install the files
+    ohai "Installing binaries, libraries and man pages..."
+    prefix.install %w[bin include lib libexec share]
+    prefix.install Dir['riscv64-unknown-elf/*']
   end
 end
 
