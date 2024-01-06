f53ef0317da3e2bbd02aa43c8d47101566168fa1
Kenny Oseleononmen <kenny1g@cs.stanford.edu>
2024-01-06 08:11:48 -0800

    Update prebuilt arm bottle

--- Formula/riscv-gnu-toolchain-13.rb
+++ Formula/riscv-gnu-toolchain-13.rb
@@ -5,7 +5,7 @@ class RiscvGnuToolchain13 < Formula
 
   if Hardware::CPU.arm?
     url "https://github.com/cs107e/homebrew-cs107e/raw/master/riscv-gnu-toolchain-arm.13.tar.gz"
-    sha256 "abdbb79e91750b1906fc2820f3ab5c133d89ce3283dec333ec7b56129f858636"
+    sha256 "6be584a4a25058bd87b8687c27d3f4625f2e0adf1b128a823d2ee3ee6c7767c7"
   else
     url "https://github.com/cs107e/homebrew-cs107e/raw/master/riscv-gnu-toolchain-intel.13.tar.gz"
     sha256 "dae252a926c582f65102b58aef1cec364bdbf5d4d8bdd2724ca9b7327fe97988"
@@ -14,8 +14,7 @@ class RiscvGnuToolchain13 < Formula
   def install
     # Homebrew extracts tarballs automatically, so just install the files
     ohai "Installing binaries, libraries and man pages..."
-    prefix.install %w[main/bin main/lib main/libexec main/share]
-    prefix.install Dir['main/riscv64-unknown-elf/*']
+    prefix.install %w[main/bin main/lib main/libexec main/share main/riscv64-unknown-elf]
   end
 end
 
